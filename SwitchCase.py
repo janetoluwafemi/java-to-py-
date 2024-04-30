87b99ea640e1ba71c5ad71e0fd24bd044e531a17
janetoluwafemi <Oluwafemijanet85@gmail.com>
2024-04-30 07:14:57 +0100

    Update SwitchCase.py

--- SwitchCase.py
+++ SwitchCase.py
@@ -1,7 +1,167 @@
+
+prompt ="""
+:::::::::::::::::::::::::::::::::::::::::::::::::
+Nokia Phone Menu
+::::::::::::::::::::::::::::::::::::::::::::::::
+1 -> Phone book
+2 -> Messages
+3 -> Chat
+4 -> Call register
+5 -> Tones
+6 -> Settings
+7 -> Call divert
+8 -> Games
+9 -> Calculator
+10 -> Reminder
+11 -> Clock
+12 -> Profiles
+13 -> SIM services
+14 -> Back
+""";
+
+String prompt2 ="""
+Phone Book
+1 -> Search
+2 -> Service Nos.
+3 -> Add name
+4 -> Erase
+5 -> Edit
+6 -> Assign tone
+7 -> Send b'card
+8 -> Options
+9 -> Speed dials
+10 -> Voice tags
+11 -> Back
+""";
+a="""
+1 -> Type of view 
+2 -> Memory status
+3 -> Back
+""";
+prompt3 = """
+Press
+1 -> Write messages
+2 -> Inbox
+3 -> Outbox
+4 -> Picture messages
+5 -> Template
+6 -> Smileys
+7 -> Message setting
+8 -> Info service
+9 -> Voice mailbox number
+10 -> Service command editor
+11 -> Back
+""";
+b="""
+1 -> Set 1
+2 -> Common
+3 -> Back
+""";
+b1="""
+1 -> Message centre number
+2 -> Message sent as
+3 -> Message validity
+4 -> Back
+""";
+b2="""
+1 -> Delivery reports
+2 -> Reply via same centre
+3 -> Character support
+4 -> Back
+""";
+c="""
+1 -> Missed calls
+2 -> Recevied calls
+3 -> Dialled number
+4 -> Erase recent call list
+5 -> Show call duration
+6 -> Show call costs
+8 -> Prepaid credit
+9 -> Back
+"""; 
+a5="""
+1 -> Last call duration
+2 -> All calls' duration
+3 -> Recieved calls' duration
+4 -> Dialled calls' duration
+5 -> Clear timers
+6 -> Back
+""";
+a6="""
+1 -> Last duration
+2 -> All calls' cost
+3 -> Clear counter
+4 -> Back
+""";
+a7="""
+1 -> Call cost limit
+2 -> Show costs in
+3 -> Back
+""";
+prompt4= """
+Tones
+press
+1 -> Ringing tone
+2 -> Ringing volume
+3 -> Incoming call alert
+4 -> Composer
+5 -> Message alert tone
+6 -> Keypad tones
+7 -> Warning and game tone
+8 -> Vibrating alert
+9 -> Screen saver
+10 -> Back
+"""; 
+prompt5= """
+press
+1 -> Call settings
+2 -> Phone settings
+3 -> Security settings
+4 -> Restore factory settings
+5 -> Back
+""";
+call="""
+1 -> Automatic redial
+2 -> Speed dialling
+3 -> Call waiting options
+4 -> Own number sending
+5 -> Phone line in use
+6 -> Automatic answer
+7 -> Back
+""";
+Phone="""
+1 -> Language
+2 -> Cell info display
+3 -> Welcome note
+4 -> Network selection
+5 -> Lights
+6 -> Confirm SIM service
+7 -> Back
+ """;
+setting="""
+1 -> PIN code request
+2 -> Call info display
+3 -> Fixed dialling
+4 -> Closed user group
+5 -> Phone security
+6 -> Change access codes
+7 -> Back
+""";
+prompt6= """
+Clock
+press
+1 -> Alarm clock
+2 -> Clock settings
+3 -> Date setting
+4 -> Stopwatch
+5 -> Countdown timer
+6 -> Auto update of date and time
+7 -> Back
+""";
 option = int(input("press between option 1 - 13"))
 match option:
-     	case 1:
-
+	case 1:
+		
             match option1:
 		case 1:
 			print("Search")
