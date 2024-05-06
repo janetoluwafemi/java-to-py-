ce56af3ea2b32299af033146cc01647fb1763503
janetoluwafemi <Oluwafemijanet85@gmail.com>
2024-05-06 01:45:02 +0100

    Create PizzaStore.java

---
+++ PizzaStore.java
@@ -0,0 +1,47 @@
+import java.util.Scanner; 
+	public class PizzaStore{
+		public static void main(String[] args){
+			Scanner scanner = new Scanner(System.in);
+
+			int sapaSize = 2000;
+			int smallMoney = 2400;
+			int bigBoys = 3000;
+			int odogwu = 4200;
+
+			int sapaSizeSlice = 4;
+			int smallMoneySlice = 6;
+			int BigBoysSlice = 8;
+			int odogwuSlice = 12;
+
+			System.out.print("How many box will you buy: ");
+			int input = scanner.nextInt();
+
+			System.out.print("How many number of people are eating: ");
+			int number = scanner.nextInt();
+		
+			System.out.print("Enter the type of pizza you want: ");
+			String nameOfPizza = scanner.nextLine(); 
+			
+
+			String pizza = """
+			pizzaType               numberOfSlices          pricePerBox
+			sapaSize              	4                	2000
+			smallMoney          	6			2400
+			bigBoys			8			3000
+			odogwu			12			4200
+			""";
+	
+			int amount = 0;
+	
+			case1 
+
+		}
+
+	}
+
+			
+			
+			
+			
+
+			
